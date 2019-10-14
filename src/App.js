ca228d99cf95546913fbc0f1401785ba2b598d99
Szasz Andrea <andrea.szdv@gmail.com>
2019-10-14 20:43:56 +0200

    simpson-quotes done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navbar from './Navbar';
-import QuoteCard from './QuoteCard';
+import Navbar from './components/Navbar';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard />
+      <QuoteList />
     </div>
   );
 }
