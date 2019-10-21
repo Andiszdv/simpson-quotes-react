bb9c97e6f263d5a966c3b1f5c5fa1b078fd29452
Szasz Andrea <andrea.szdv@gmail.com>
2019-10-21 12:19:17 +0200

    new characters of simpson family with new quotes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteList from './components/QuoteList';
+import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.min.css";
 
 function App() {
   return (
--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -6,37 +6,38 @@ import QuoteCard from './QuoteCard';
 const quotes = [
   {
     quote:
-   "Talk is cheap. Show me the code.",
-    character: "Jacob Johnson",
+      "Facts are meaningless. You can use facts to prove anything that’s even remotely true. Facts schmacts.",
+    character: "The Simpsons",
     image:
-    "https://randomuser.me/api/portraits/men/24.jpg"
-  },
-  {
-    quote: "when you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.",
-    character: "Clara Graham",
-    image:
-    "https://randomuser.me/api/portraits/women/57.jpg"
-  },
-  {
-    quote: "Programs must be written for people to read, and only incidentally for machines to execute.",
-    character: "Sebastian Richards",
-    image:
-    "https://randomuser.me/api/portraits/men/33.jpg"
+      "https://www.thewrap.com/wp-content/uploads/2015/11/The-Simpsons-family-couch.png"
   },
   {
     quote:
-      "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
-    character: "Berta Little",
+   "It takes two to lie. One to lie, and one to listen.",
+    character: "Homer Simpson",
     image:
-      "https://randomuser.me/api/portraits/women/16.jpg"
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyq8ftD06bq45T3O9_Rx9lae30NQduSaS2_m_r5Gxzl4RlvNhQ&s"
+  },
+  {
+    quote: "As long as everybody is videotaping everybody else, justice will be done.",
+    character: "Maude Flanders",
+    image:
+    "https://i2-prod.mirror.co.uk/incoming/article5851553.ece/ALTERNATES/s615b/ustv-the-simpsons-maude-flanders-02.jpg"
+  },
+  {
+    quote: "Most of you will never fall in love, and marry out of fear of dying alone.",
+    character: "Ned Flanders",
+    image:
+    "https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/ppqf3a60rzdblmkwkex6/best-simpsons-characters-lisa-simpson"
   },
   {
     quote:
-      "Is it possible that software is not like anything else, that it is meant to be discarded: that the whole point is to always see it as a soap bubble?",
-    character: "Carla Szabo",
+      "But surely you can’t put a price on family.I wouldn’t have thought so either, but here we are.",
+    character: "Charles Montgomery",
     image:
-      "https://randomuser.me/api/portraits/women/79.jpg"
+      "https://cinemathread.com/wp-content/uploads/2017/02/the-old-man-and-the-lisa-season-8-episode-21.jpg"
   }
+  
 ];
 
 const QuoteList = () => (
